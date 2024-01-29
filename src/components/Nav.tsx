4fac3e6d5dbacf52ecf5264730149910e9b898d9
Hannah Thorley <hannahthorley2014@gmail.com>
2024-01-29 14:55:07 +0000

    nav link test

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,25 +4,25 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="sm:absolute sm:right-10 top-10 pb-3 md:block md:pb-0 md:mt-0 sm:text-xl text-sm flex flex-row justify-center font-light">
+    <nav className="sm:absolute sm:right-10 top-10 pb-3 md:block md:pb-0 md:mt-0 sm:text-xl text-base flex flex-row justify-center font-light">
       <Link
         className="cursor-pointer highlight hover:font-semibold tracking-wide hover:tracking-normal  mx-2"
         aria-label="projects"
-        href="#projects"
+        href="/#projects"
       >
         projects
       </Link>
       <Link
         className="cursor-pointer highlight hover:font-semibold  tracking-wide hover:tracking-normal  mx-2"
         aria-label="about"
-        href="#about"
+        href="/#about"
       >
         about
       </Link>
       <Link
         className="cursor-pointer highlight hover:font-semibold  tracking-wide hover:tracking-normal   mx-2"
         aria-label="contact"
-        href="#contact"
+        href="/#contact"
       >
         contact
       </Link>
